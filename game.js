94d05b59d025a8dd552f7298208b508ae0306920
Tony <lam_273@hotmail.com>
2022-12-17 16:55:14 -0500

    Initial Styling

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let boardBot = document.getElementById('board-bot');
 let resultHistory = [];
 let score = 0;
 let gameOptions = {
-    dificulty: undefined,
+    time: 15000,
     numRange: 10,
     mathOperations: ['sum', 'res', 'multi', 'div']
 };
@@ -11,38 +11,65 @@ let gameOptions = {
 START PAGE
 ###########*/
 let startPage = () => {
-/*FUNCTION VARIABLES*/
+    /*FUNCTION VARIABLES*/
 
 
-/*BOARD TOP*/
+    /*BOARD TOP*/
     let titleTest = document.createElement('h1');
     titleTest.textContent = 'TESTING TITLE'
 
-/*BOARD BOTTOM*/
+    /*BOARD BOTTOM*/
     let startBtn = document.createElement('button');
     startBtn.classList.add('button');
     startBtn.setAttribute('id', 'start-btn');
     startBtn.setAttribute('type', 'button');
-    startBtn.setAttribute('onclick', 'gameStart()')
+    startBtn.setAttribute('onclick', 'countdown()')
     startBtn.textContent = 'START GAME';
 
-/*ELEMENT RENDER*/
+    /*ELEMENT RENDER*/
     boardTop.replaceChildren(titleTest)
     boardBot.replaceChildren(startBtn);
 }
 
 /*#########
+COUNTDOWN BEFORE GAMESTART PAGE
+###########*/
+let countdown = () => {
+
+    /*BOARD TOP*/
+    let startCounter = document.createElement('div');
+    let timeDisplay = document.createElement('span');
+    startCounter.classList.add('countdown');
+    startCounter.appendChild(timeDisplay);
+    timeDisplay.classList.add('countdown-number');
+    timeDisplay.textContent = 3; //CHANGED TO 1 FOR DEV PURPOSES! DEFAULT IS 3
+
+    /*ELEMENT RENDER*/
+    boardTop.replaceChildren(startCounter);
+    boardBot.replaceChildren();
+
+    /*FUNCTION CALLBACK*/
+    let interval = setInterval(() => {
+        if (timeDisplay.textContent <= 1) {
+            gameStart()
+            clearInterval(interval)
+        } else { timeDisplay.textContent -= 1 }
+    }, 1000)
+}
+
+
+/*#########
 GAME START PAGE
 ###########*/
 let gameStart = () => {
-/*FUNCTION VARIABLES*/
+    /*FUNCTION VARIABLES*/
     let activeOperators = gameOptions.mathOperations;
     let numRegex = /^\-*[0-9]+(.[0-9]*)?$/;
     let correctAnswer, inputAnswer;
     let numGen0 = () => { //for numbers 0 and up
         return Math.round(Math.random() * gameOptions.numRange)
     }
-    let numGen1 =  ()  => { //for numbers 1 and up
+    let numGen1 = () => { //for numbers 1 and up
         return Math.ceil(Math.random() * gameOptions.numRange)
     }
 
@@ -52,69 +79,78 @@ let gameStart = () => {
         let opeRandom = Math.round(Math.random() * (activeOperators.length - 1));
         switch (activeOperators[opeRandom]) {
             case 'sum':
-            a = numGen0();
-            b = numGen0();
-            correctAnswer = a + b;
-            mathText.textContent = `${a} + ${b}`;
-            break;
-        case 'res':
-            a = numGen0();
-            b = numGen0();
-            correctAnswer = a - b;
-            mathText.textContent = `${a} - ${b}`;
-            break;
-        case 'multi':
-            a = numGen0();
-            b = numGen0();
-            correctAnswer = a * b;
-            mathText.textContent = `${a} x ${b}`
-            break;
-        case 'div':
-            a = numGen1();
-            b = numGen1();
-            if ((a % b) != 0) { 
-                correctAnswer = parseFloat((a / b).toFixed(2)) //TODO: FIX ROUNDING ISSUE 
-            } else { 
-                correctAnswer = a / b 
-            };
-            mathText.textContent = `${a} ÷ ${b}`;
-            break;
-        default:
-            console.log(`something went wrong`);
+                a = numGen0();
+                b = numGen0();
+                correctAnswer = a + b;
+                mathText.textContent = `${a} + ${b}`;
+                break;
+            case 'res':
+                a = numGen0();
+                b = numGen0();
+                correctAnswer = a - b;
+                mathText.textContent = `${a} - ${b}`;
+                break;
+            case 'multi':
+                a = numGen0();
+                b = numGen0();
+                correctAnswer = a * b;
+                mathText.textContent = `${a} x ${b}`
+                break;
+            case 'div':
+                a = numGen1();
+                b = numGen1();
+                if ((a % b) != 0) {
+                    correctAnswer = parseFloat((a / b).toFixed(2)) //TODO: FIX ROUNDING ISSUE 
+                } else {
+                    correctAnswer = a / b
+                };
+                mathText.textContent = `${a} ÷ ${b}`;
+                break;
+            default:
+                console.log(`something went wrong`);
         }
     }
-    
+
     //Answer Check
-    function answerCheck (val1, val2) {
+    function answerCheck(val1, val2) {
         if (val1 == val2) {
             score++;
             return 'correct'
-        } else {return 'wrong'}
+        } else { return 'wrong' }
     }
-    
-/*BOARD TOP*/
-    let timeBar = document.createElement('h1');
-    timeBar.textContent = 'INSERT TIMER BAR HERE';
-    let mathText = document.createElement('p');
+
+    /*BOARD TOP*/
+    let timeBar = document.createElement('div');
+    let timeRem = document.createElement('div');
+    timeBar.classList.add('time-bar');
+    timeBar.appendChild(timeRem);
+    timeRem.classList.add('time-rem');
+
+    let mathText = document.createElement('span');
+    mathText.classList.add('math-text');
     mathGen()
 
 
-/*BOARD BOTTOM*/
+    /*BOARD BOTTOM*/
     let inputArea = document.createElement('input');
     inputArea.setAttribute('id', 'user-answer');
 
     inputArea.addEventListener('keypress', (event) => {
-        if(event.key === "Enter") {
+        if (event.key === "Enter") {
             inputAnswer = +(inputArea.value);
             //console.log(inputAnswer);
             if (numRegex.test(inputAnswer)) {
                 event.preventDefault();
                 //console.log(`${mathText.textContent}`)
                 console.log(`input is ${inputAnswer}, answer is ${correctAnswer}`);
-                //console.log(answerCheck(inputAnswer, correctAnswer));
-                resultHistory.push(`${mathText.textContent} = ${inputAnswer} ${answerCheck(inputAnswer, correctAnswer)}`)
+                resultHistory.push(
+                    {
+                        mathText: mathText.textContent,
+                        userAnswer: inputAnswer,
+                        answerResult: answerCheck(inputAnswer, correctAnswer)
+                    }
+                )
                 console.log(`the score is ${score}`)
-                //console.log(resultHistory);
                 inputArea.value = "";
                 mathGen()
             } else {
@@ -124,49 +160,51 @@ let gameStart = () => {
         }
     })
 
-    let resultBtn = document.createElement('button');
-    resultBtn.classList.add('button');
-    resultBtn.setAttribute('type', 'button');
-    resultBtn.setAttribute('id', 'result-button');
-    resultBtn.textContent = 'Show Rersults';
-    resultBtn.setAttribute('onclick', 'resultWindow()')
-
-/*ELEMENT RENDER*/
+    /*ELEMENT RENDER*/
     boardTop.replaceChildren(timeBar, mathText);
-    boardBot.replaceChildren(inputArea, resultBtn);
+    boardBot.replaceChildren(inputArea);
+    inputArea.focus(); //To type immediately in the answer box
 
+    /*FUNCTION CALLBACK 1min TIMER*/
+    let time = gameOptions.time;
+    timeRem.style.animationDuration = `${time / 1000}s`
+
+    setTimeout(() => {
+        resultWindow();
+    }, time);
 }
-
-
 
 
 /*#########
 RESULT PAGE
 ###########*/
 let resultWindow = () => {
-/*FUNCTION VARIABLES*/
+    /*FUNCTION VARIABLES*/
 
-/*BOARD TOP*/
+    /*BOARD TOP*/
     let resultTitle = document.createElement('h1');
-    resultTitle.classList.add('title');
+    resultTitle.classList.add('result-title');
     resultTitle.textContent = 'Results';
 
     let resultList = document.createElement('ul');
+    resultList.classList.add('result-list')
 
     resultHistory.forEach(result => {
         let answerLine = document.createElement('li');
         resultList.appendChild(answerLine);
         answerLine.classList.add('answer');
-        answerLine.textContent = result;
+        answerLine.textContent = `${result.mathText} = ${result.userAnswer}`;
+        answerLine.setAttribute('data-result', `${result.answerResult}`)
     })
 
     let scoreResult = document.createElement('p');
-    scoreResult.textContent = score;
+    scoreResult.classList.add('result-text');
+    scoreResult.textContent = `Your score is: ${score}`;
     //Clear scores and results
     score = 0;
     resultHistory = [];
 
-/*BOART BOTTOM*/
+    /*BOART BOTTOM*/
     let returnBtn = document.createElement('button');
     returnBtn.classList.add('button');
     returnBtn.setAttribute('type', 'button');
@@ -174,7 +212,7 @@ let resultWindow = () => {
     returnBtn.textContent = 'Return to main menu';
     returnBtn.setAttribute('onclick', 'startPage()');
 
-/*ELEMENT RENDER*/
+    /*ELEMENT RENDER*/
     boardTop.replaceChildren(resultTitle, resultList, scoreResult)
     boardBot.replaceChildren(returnBtn);
 }
